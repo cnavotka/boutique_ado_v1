3e08623136232a4f146fc06224eaea8317c1681d
Carlos Navotka <knavotka@hotmail.com>
2021-10-14 16:34:22 +0000

    Fix bug in products views.py that was causing an error

--- bag/views.py
+++ bag/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
+
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -22,4 +24,3 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
-
