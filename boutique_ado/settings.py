c0f6ef9a9100115583e848d7a2e402cc563b9a14
Carlos Navotka <knavotka@hotmail.com>
2021-11-16 19:22:50 +0000

    Start deploy in Heroku app

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -120,11 +121,15 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://jpevvexqqlmjki:d5a7429ade3119807657bb78de3055a53f1e331ce02b2f91645e4e4453cc13b1@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dal2ih0rinam9u')
 }
 
 
