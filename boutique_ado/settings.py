5dcb112062a0d1cd4627a336aa760629ade2bd21
Carlos Navotka <knavotka@hotmail.com>
2021-11-16 22:48:04 +0000

    Deply app in Heroku, did migrations and update the uncommented code in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -121,15 +121,11 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
-
 DATABASES = {
-    'default': dj_database_url.parse('postgres://jpevvexqqlmjki:d5a7429ade3119807657bb78de3055a53f1e331ce02b2f91645e4e4453cc13b1@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dal2ih0rinam9u')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3',
+    }
 }
 
 
